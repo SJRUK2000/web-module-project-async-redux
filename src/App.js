78da9277ad6e7c0d446dbd9ac7bd813935ee8e7a
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-04-21 11:15:22 -0400

    fixed ESLINST err; change ecmaVersion : 8 Skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import GifList from  './componenets/GifList'
+import GifForm from './componenets/GifForm';
+
 import './App.css';
 
-function App() {
+function App(props) {
+   const { loading, gifs } = props;
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Search For Gifs</h1>
+      <GifForm/>
+      {
+        loading ? <h3>Loading on up!</h3> : <GifList gifs={gifs}/>
+        }
+
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    gifs: state.gifs,
+    loading: state.loading
+  }
+}
+
+export default connect(mapStateToProps)(App);
